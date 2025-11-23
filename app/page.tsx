0556d154bb284adbd5c5e62317d51f9b153adcbb
hello-yuki0409 <bobu2784@gmail.com>
2025-11-23 22:07:29 +0900

    chore: DaisyUIを導入

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ export default function Home() {
   return (
     <div><h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1></div>
+    </h1>
+    <button
+  className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+    </div>
   );
 }
