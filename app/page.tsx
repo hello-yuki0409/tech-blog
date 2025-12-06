96ad50169785265f860c0cdad57dd5e627da417d
hello-yuki0409 <bobu2784@gmail.com>
2025-12-06 23:42:44 +0900

    feat: H1だけ

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,7 @@ export default function Home() {
   return (
     <div>
       <Title text="Hello world!" />
-      <button
-        className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
-      >
-        Button
-      </button>
+      <h1>個人記事一覧</h1>
     </div>
   );
 }
